81202abbcb3ba61d451ea92feaa81f4bc3967f07
acrviper21 <42522349+acrviper21@users.noreply.github.com>
2025-10-11 22:54:31 -0500

    Setup server connection

--- WebServer.py
+++ WebServer.py
@@ -5,17 +5,37 @@ import os
 load_dotenv()
 
 def start_server():
+    max_clients = 1   
+
     # Get PC name
     host = socket.gethostname()
     print(f"The name of the local host is {host}")
     # Get IP from PC name
-    IP = socket.gethostbyname(host)
-    print(f"IP address of the localhost is {IP}")
+    SERVER_IP = socket.gethostbyname(host)
+    print(f"IP address of the localhost is {SERVER_IP}")
 
     # Port number for server to run on
-    port = 8080
+    SERVER_PORT = os.getenv("SERVER_PORT")
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((SERVER_IP, SERVER_PORT))
+
+    # How many clients the server can listen for simultaneously
+    server_socket.listen(max_clients)
+
+    conn, address = server_socket.accept()
+
+    while True:
+        # Receive data stream
+        data = conn.recv(1024).decode()
+        if not data:
+            break
+        print(f"{"user"}: {str(data)}")
+        # Send data back to client
+        conn.send(data.encode())
+
+    # Close connection
+    conn.close()
 
 if __name__ == '__main__':
     start_server()
