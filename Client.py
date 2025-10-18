983d8d4109c9af06253eb073cff8b1e658776907
acrviper21 <42522349+acrviper21@users.noreply.github.com>
2025-10-17 20:34:21 -0500

    allow user to create username and add a exit function

--- Client.py
+++ Client.py
@@ -2,13 +2,34 @@ import socket
 from dotenv import load_dotenv
 import os 
 
+load_dotenv()
+
 def start_client():
+    #Allow user to choose a username
     username = input("Hello, please enter a username: ")
-    print(username)
+    
     SERVER_IP = os.getenv("SERVER_IP")
     SERVER_PORT = os.getenv("SERVER_PORT")
     client_socket = socket.socket()
-    client_socket.connect((SERVER_IP, SERVER_PORT))
+    client_socket.connect((SERVER_IP, int(SERVER_PORT)))
+
+    client_socket.send(username.encode())
+
+    message = input(f"{username} -> ")
+
+    while True:
+        client_socket.send(message.encode())
+        data = client_socket.recv(1024).decode()
+
+        if message.lower().strip() == "exit":
+            print(f"Server: {data}")
+            break
+
+        print(f"Server: {data}")
+        message = input(f"{username} -> ")
+
+    # Close connections
+    client_socket.close()
 
 
 if __name__ == '__main__':
